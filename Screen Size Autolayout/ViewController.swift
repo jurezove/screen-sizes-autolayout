ecfd9e0a0e2e3520972684c0ebacf9a294d32bbf
Jure Žove <jure.pase.kure@gmail.com>
2015-05-09 11:27:55 +0200

    Initial Commit

---
+++ Screen Size Autolayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Screen Size Autolayout
+//
+//  Created by Jure Zove on 09/05/15.
+//  Copyright (c) 2015 Candy Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
