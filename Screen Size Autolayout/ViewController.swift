9d2c114830d1aa97f4e1681788cdc8e0cc72c019
Jure Žove <jure.pase.kure@gmail.com>
2015-05-10 12:05:27 +0200

    initial commit

--- Screen Size Autolayout/ViewController.swift
+++ Screen Size Autolayout/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var carDescription: UILabel!
+    
+    @IBOutlet weak var imageHeightConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.imageView.image = UIImage(named: "Zonda")
+        self.name.text = "Zonda R"
+        self.price.text = "$2.9 Million"
+        self.carDescription.text = "The Zonda R debuted at the Geneva Motor Show 2007, using the 6.0 litre V12 M120 engine sourced from the race version of the Mercedes-Benz CLK-GTR."
+        
+        self.imageHeightConstraint.constant = imageHeightConstraintConstant()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func screenHeight() -> CGFloat {
+        return UIScreen.mainScreen().bounds.height;
+    }
 
+    func imageHeightConstraintConstant() -> CGFloat {
+        switch(self.screenHeight()) {
+        case 568:
+            return 340
+            
+        case 667:
+            return 460
+            
+        default:
+            return 250
+        }
+    }
 }
 
