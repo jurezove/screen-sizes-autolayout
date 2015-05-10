eae0cc5a34d987a5ab21b02b66839900c7f26d59
Jure Žove <jure.pase.kure@gmail.com>
2015-05-10 12:17:07 +0200

    best solution without code

--- Screen Size Autolayout/ViewController.swift
+++ Screen Size Autolayout/ViewController.swift
@@ -24,30 +24,11 @@ class ViewController: UIViewController {
         self.name.text = "Zonda R"
         self.price.text = "$2.9 Million"
         self.carDescription.text = "The Zonda R debuted at the Geneva Motor Show 2007, using the 6.0 litre V12 M120 engine sourced from the race version of the Mercedes-Benz CLK-GTR."
-        
-        self.imageHeightConstraint.constant = imageHeightConstraintConstant()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-    func screenHeight() -> CGFloat {
-        return UIScreen.mainScreen().bounds.height;
-    }
-
-    func imageHeightConstraintConstant() -> CGFloat {
-        switch(self.screenHeight()) {
-        case 568:
-            return 340
-            
-        case 667:
-            return 460
-            
-        default:
-            return 250
-        }
     }
 }
 
